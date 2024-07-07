0080c36749a936a7288dd5d1ecf99d0e3d4117c3
ankys8820 <ankys8820@outlook.com>
2024-07-07 18:52:23 +0530

    backend initialised

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import products from "./data/products.js";
 
+const app = express();
+
+const port = 5000 || process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+//
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
