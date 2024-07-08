684ef9bb09ed27b0f2d942d98ec25ddf90c5a896
ankys8820 <ankys8820@outlook.com>
2024-07-08 17:31:50 +0530

    fetch data from backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import products from "./data/products.js";
+import cors from "cors";
 
 const app = express();
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
